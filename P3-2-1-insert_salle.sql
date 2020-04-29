5fe5dd4fa76cf27baebc7bb4ea78ae97761cf8ac
Clement Dhaisne <clement.dhaisne@iut-rodez.fr>
2020-04-29 13:32:43 +0200

    fix #0.4 Ajout feuille de TP 4

--- P3-2-1-insert_salle.sql
+++ P3-2-1-insert_salle.sql
@@ -1,11 +1,11 @@
-CREATE FUNCTION gds.insert_salle (un_nom varchar(250), un_nb_personnes_max integer) RETURNS gds.salle AS $$
-    INSERT INTO gds.salle (nom,nb_personnes_max,date_creation,date_modification,id)
-    VALUES (un_nom,un_nb_personnes_max,now(),now(),nextval('gds.salle_seq'));
-    SELECT * FROM gds.salle
+  
+-- Fonction insert salle
+
+CREATE OR REPLACE FUNCTION gds.insert_salle(
+        un_nom VARCHAR(250), 
+        un_nb_personnes_max integer) 
+RETURNS gds.salle AS $$
+        -- insert la salle
+        INSERT INTO gds.salle (id, nom, nb_personnes_max, date_creation, date_modification) 
+        VALUES (nextval('gds.salle_seq'),un_nom, un_nb_personnes_max, now(), now()) returning *;
 $$ LANGUAGE SQL;
-
-select * from gds.insert_salle('paquerette', 12);
-select * from gds.insert_salle('rose', 6);
-select * from gds.insert_salle('dalhia', 24);
-select * from gds.insert_salle('tulipe', 32);
-
--- P3-2-2-insert_reservation.sql
+++ P3-2-2-insert_reservation.sql
@@ -1,12 +1,25 @@
-CREATE FUNCTION gds.insert_reservation (un_date_debut timestamp without time zone, un_date_fin timestamp without time zone, un_nom_de_salle varchar(250)) RETURNS gds.salle AS $$
-    INSERT INTO gds.reservation (date_debut,date_fin,date_creation,date_modification,salle_id)
-    VALUES (un_date_debut, un_date_fin, un_nom_de_salle, now(), now(), nextval('gds.salle_seq'));
-    SELECT * FROM gds.reservation
+-- Fonction recuperant l'id d'une salle à partir de son nom
+
+CREATE OR REPLACE FUNCTION gds.get_salle_id(un_nom_de_salle character varying(250)) 
+RETURNS bigint AS $$
+	select id from gds.salle where nom = un_nom_de_salle;
 $$ LANGUAGE SQL;
 
-select * from gds.insert_reservation('2020-04-24 15:00:00','2020-04-24 17:00:00','paquerette');
-select * from gds.insert_reservation('2020-04-22 15:00:00','2020-04-22 17:00:00','paquerette');
-select * from gds.insert_reservation('2020-04-22 12:00:00','2020-04-22 14:00:00','rose');
-select * from gds.insert_reservation('2020-04-23 18:00:00','2020-04-22 19:00:00','tulipe');
-select * from gds.insert_reservation('2020-04-25 14:00:00','2020-04-25 17:00:00','dalhia');
-select * from gds.insert_reservation('2020-04-22 12:00:00','2020-04-22 14:00:00','dalhia');
+-- Fonction insérant une nouvelle réservation
+
+CREATE OR REPLACE FUNCTION gds.insert_reservation(
+    une_date_debut timestamp without time zone,
+    une_date_fin timestamp without time zone, 
+    un_nom_de_salle character varying(250) 
+) RETURNS gds.reservation AS $$
+        -- insert la reservation
+        INSERT INTO gds.reservation (id, date_debut, date_fin, date_creation, date_modification,salle_id) 
+        VALUES (nextval('gds.reservation_seq'),
+				une_date_debut, 
+				une_date_fin, 
+				now(), 
+				now(),
+				gds.get_salle_id(un_nom_de_salle)
+			   ) 
+        returning *;
+$$ LANGUAGE SQL;
